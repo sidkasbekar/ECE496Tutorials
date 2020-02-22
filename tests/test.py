a769b47211da1f376a9baca459b46a16135f5a43
anurag1457 <anurag1457@gmail.com>
2020-02-21 22:04:28 -0500

    added instructions to tests

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,13 @@ import unittest
 class TestCases(unittest.TestCase):
 
 	def test_sanity(self):
-		self.assertEqual(1, 2)
+		self.assertEqual(1, 1)
+
+
+	'''
+	Add your own test cases here
+	function name must start with test_
+	'''
 
 if __name__ == "__main__":
 	unittest.main()
