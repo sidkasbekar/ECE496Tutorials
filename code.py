8014dbd2b6c56bac65061697361036c932e46ec3
anurag1457 <anurag1457@gmail.com>
2020-02-21 22:19:24 -0500

    lint

--- code.py
+++ code.py
@@ -1,10 +1,9 @@
-import numpy as np
-
-
 def add(a,b):
     return a+b
 
-'''
-    Write a meaningful function here, unlike the one above
+def no_op():
+    '''
+        Write a meaningful function here, unlike the one above
 
-    Then, add a test case for this function in tests/test.py'''
+        Then, add a test case for this function in tests/test.py'''
+    return
