7e43335904f994014b5b552c715239d5c093545d
anurag1457 <anurag1457@gmail.com>
2020-02-21 21:55:43 -0500

    changed test case

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ import unittest
 
 class TestCases(unittest.TestCase):
 
-	def sanity_check():
-		assert 1==2
+	def sanity_check(self):
+		self.assertEqual(1, 2)
 
 if __name__ == "__main__":
 	unittest.main()
